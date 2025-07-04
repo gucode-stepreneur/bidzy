454ea9a6bf5cde5f96a67d8e08c0dcb57a1a4291
Stepreneur <kfranzaza13@gmail.com>
2025-07-05 04:15:08 +0700

    addd test

---
+++ src/app/test/page.js
@@ -0,0 +1,14 @@
+'use client'
+import { useSession } from "next-auth/react";
+
+export default function Profile() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <p>Loading...</p>;
+
+  if (status === "authenticated") {
+    return <p>Welcome {session.user.name}</p>;
+  }
+
+  return <p>Please login</p>;
+}
